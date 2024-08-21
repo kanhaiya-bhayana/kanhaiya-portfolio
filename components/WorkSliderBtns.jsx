ff37359343f9cac5aa710653e2527e87efe1c3fb
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2024-08-21 12:59:49 +0530

    Work: WorkSliederBtn updated

--- components/WorkSliderBtns.jsx
+++ components/WorkSliderBtns.jsx
@@ -1,17 +1,17 @@
 "use client";
 
 import { useSwiper } from "swiper/react";
-import { IoChevronForwardCircleOutline } from "react-icons/io5";
-import { IoArrowBackCircleOutline } from "react-icons/io5";
+
+import { PiCaretLeftBold, PiCaretRightBold} from 'react-icons/pi'
 
 const WorkSliderBtns = () => {
     const swiper = useSwiper();
     console.log("Swiper: ", swiper);
     
   return (
-    <div className="flex flex-row gap-4 text-4xl">
-        <button><IoArrowBackCircleOutline className="hover:text-accent" onClick={() => swiper.slidePrev()} /></button>
-        <button><IoChevronForwardCircleOutline className="hover:text-accent" onClick={() => swiper.slideNext()} /></button>
+    <div className="flex gap-2 text-xl absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none">
+        <button><PiCaretLeftBold className="hover:text-accent" onClick={() => swiper.slidePrev()} /></button>
+        <button><PiCaretRightBold className="hover:text-accent" onClick={() => swiper.slideNext()} /></button>
     </div>
   )
 }
